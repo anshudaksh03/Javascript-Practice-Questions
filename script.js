a6ccbd3011fa09f8cdc038836927cdf654a82bbc
AnnShu <anshudakah03@gmail.com>
2026-01-22 17:43:18 +0000

    question 1

---
+++ script.js
@@ -0,0 +1,8 @@
+// Variable and data type Question
+// Q.1-Declare a variable  using let and log its value
+// const a = 12;
+// console.log(a)
+
+// Q.2- Create a constant to store  the value of PI anmd log it;
+const pi = Math.PI;
+console.log(pi)
